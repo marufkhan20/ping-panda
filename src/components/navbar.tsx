fe124f11fc8999cb5c89a535102c331046d77856
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-11-14 00:00:43 +0600

    add authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,18 @@
 import { SignOutButton } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs/server"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 import { MaxWidthWrapper } from "./max-width-wrapper"
 import { Button, buttonVariants } from "./ui/button"
 
-export const Navbar = () => {
-  const user = false
+export const Navbar = async () => {
+  const user = await currentUser()
 
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between">
-          <Link className="flex z-40 font-semibold" href="/">
+          <Link href="/" className="flex z-40 font-semibold">
             Ping<span className="text-brand-700">Panda</span>
           </Link>
 
@@ -37,7 +38,7 @@ export const Navbar = () => {
             ) : (
               <>
                 <Link
-                  href="/dashboard"
+                  href="/pricing"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
