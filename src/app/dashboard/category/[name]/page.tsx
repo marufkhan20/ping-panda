83e0021110b8ea1fa6767e94af140fee923e450d
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-11-22 02:06:31 +0600

    Change runtime edge to nodejs

---
+++ src/app/dashboard/category/[name]/page.tsx
@@ -0,0 +1,11 @@
+interface CategoryDetailsPageProps {
+  params: {
+    name: string | string[] | undefined
+  }
+}
+
+const CategoryDetailsPage = ({ params }: CategoryDetailsPageProps) => {
+  return <div>CategoryDetailsPage</div>
+}
+
+export default CategoryDetailsPage
