999966c74c55845e5cc3aa47d677ff01f1a51ec5
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-11-23 02:01:04 +0600

    implement upgrade plan page

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,21 @@
-import { Hono } from "hono"
-import { cors } from "hono/cors"
-import { handle } from "hono/vercel"
-import { authRouter } from "./routers/auth-router"
-import { categoryRouter } from "./routers/category-router"
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+import { authRouter } from "./routers/auth-router";
+import { categoryRouter } from "./routers/category-router";
+import { paymentRouter } from "./routers/payment-router";
+import { projectRouter } from "./routers/project-router";
 
-const app = new Hono().basePath("/api").use(cors())
+const app = new Hono().basePath("/api").use(cors());
 
 const appRouter = app
   .route("/auth", authRouter)
   .route("/category", categoryRouter)
+  .route("/payment", paymentRouter)
+  .route("/project", projectRouter);
 
-export const httpHandler = handle(app)
+export const httpHandler = handle(app);
 
-export default app
+export default app;
 
-export type AppType = typeof appRouter
+export type AppType = typeof appRouter;
