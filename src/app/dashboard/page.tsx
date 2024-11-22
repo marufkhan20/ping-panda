ff422ed69ee4ab746372a8ae0407e6358d56f13c
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-11-23 02:14:43 +0600

    Implement payment success modal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,42 +1,67 @@
-import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
-import { DashboardPage } from "@/components/dashboard-page"
-import { Button } from "@/components/ui/button"
-import { db } from "@/db"
-import { currentUser } from "@clerk/nextjs/server"
-import { PlusIcon } from "lucide-react"
-import { redirect } from "next/navigation"
-import { DashboardPageContent } from "./dashboard-page-content"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal";
+import { DashboardPage } from "@/components/dashboard-page";
+import { PaymentSuccessModal } from "@/components/payment-success-modal";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { createCheckoutSession } from "@/lib/stripe";
+import { currentUser } from "@clerk/nextjs/server";
+import { PlusIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+import { DashboardPageContent } from "./dashboard-page-content";
 
-const Dashboard = async () => {
-  const auth = await currentUser()
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const auth = await currentUser();
 
   if (!auth) {
-    redirect("/sign-in")
+    redirect("/sign-in");
   }
 
   const user = await db.user.findUnique({
-    where: {
-      externalId: auth.id,
-    },
-  })
+    where: { externalId: auth.id },
+  });
 
   if (!user) {
-    redirect("/sign-in")
+    return redirect("/welcome");
   }
-  return (
-    <DashboardPage
-      cta={
-        <CreateEventCategoryModal>
-          <Button className="w-full">
-            <PlusIcon className="size-4 mr-2" /> Add Category
-          </Button>
-        </CreateEventCategoryModal>
-      }
-      title="Dashboard"
-    >
-      <DashboardPageContent />
-    </DashboardPage>
-  )
-}
 
-export default Dashboard
+  const intent = searchParams.intent;
+
+  if (intent === "upgrade") {
+    const session = await createCheckoutSession({
+      email: user.email,
+      userId: user.id,
+    });
+
+    if (session.url) redirect(session.url);
+  }
+
+  const success = searchParams.success;
+
+  return (
+    <>
+      {success ? <PaymentSuccessModal /> : null}
+
+      <DashboardPage
+        cta={
+          <CreateEventCategoryModal>
+            <Button className="w-full sm:w-fit">
+              <PlusIcon className="size-4 mr-2" />
+              Add Category
+            </Button>
+          </CreateEventCategoryModal>
+        }
+        title="Dashboard"
+      >
+        <DashboardPageContent />
+      </DashboardPage>
+    </>
+  );
+};
+
+export default Page;
